db3f6d20ec904907d0e0574baa9e14bdbd6b3621
Gabor Csardi <csardi.gabor@gmail.com>
2014-09-12 12:47:40 -0400

    Finetune seer firewall

--- seer.rpkg.org/script/secure.sh
+++ seer.rpkg.org/script/secure.sh
@@ -1,21 +1,6 @@
 #! /bin/bash -exu
 
 ufw allow ssh
-ufw allow http
-ufw allow 5984
+ufw allow 9200
 ufw --force enable
-
-cat >/tmp/FWD <<EOF
-# Forward http to CouchDB
-*nat
-:PREROUTING ACCEPT [0:0]
--A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5984
-COMMIT
-
-EOF
-
-cp /etc/ufw/before.rules /etc/ufw/before.rules.bak
-cat /tmp/FWD /etc/ufw/before.rules >/tmp/FWD2 && 
-cp /tmp/FWD2 /etc/ufw/before.rules
-
 ufw reload
